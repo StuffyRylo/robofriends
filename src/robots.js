15877988a9c3d847eeb2eba8a9d5403f96a0b7ff
silas terdoo <silasterdoo@gmail.com>
2021-10-22 13:38:10 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Ayce Icon',
+	username: 'Icon',
+	email: 'Icon@gmail.com'
+},
+{
+	id: 2,
+	name: 'Ahemen Kelvin',
+	username: 'Kemikalz',
+	email: 'Kemikalz@gmail.com'
+},
+{
+	id: 3,
+	name: 'Francis Abi',
+	username: 'kpanku',
+	email: 'OJsuprises@gmail.com'
+},
+{
+	id: 4,
+	name: 'Wanger Kelivin',
+	username: 'Truth',
+	email: 'Truth@gmail.com'
+},
+{
+	id: 5,
+	name: 'Chia Eric',
+	username: 'Jazz',
+	email: 'Jazz@gmail.com'
+},
+{
+	id: 6,
+	name: 'Weka Victor',
+	username: 'AttAll',
+	email: 'AttAll@gmail.com'
+},
+{
+	id: 7,
+	name: 'Asogo Marcel',
+	username: 'AsogoPikin',
+	email: 'Marcel@gmail.com'
+},
+{
+	id: 8,
+	name: 'Iorliam Ephriam',
+	username: 'Fatboi',
+	email: 'fatboi@gmail.com'
+},
+{
+	id: 9,
+	name: 'Nongo Raphael',
+	username: 'Raxin',
+	email: 'Raxin@gmail.com'
+},
+{
+	id: 10,
+	name: 'Silas Dooter',
+	username: 'Valid',
+	email: 'Valid@gmail.com'
+},
+{
+	id: 11,
+	name: 'Silas Doose',
+	username: 'Stuffie',
+	email: 'Doose@gmail.com'
+},
+]
---
+++ src/stuff.js
@@ -0,0 +1,8 @@
+// const Song = (props) => {
+// 	console.log(props)
+// 	return(
+// 		<h1>do re mi fa so la ti do{props.name}</h1>
+// 		);
+// }
+
+// export default Song;
